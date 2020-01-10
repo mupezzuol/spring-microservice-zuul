32dd1000fe50786fe66034bfc23ae0cf8f8938df
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-10 19:05:53 -0300

    Add Config Default Zuul for API Gateway

--- src/main/java/br/com/microservice/zuul/SpringMicroserviceZuulApplication.java
+++ src/main/java/br/com/microservice/zuul/SpringMicroserviceZuulApplication.java
@@ -2,8 +2,10 @@ package br.com.microservice.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class SpringMicroserviceZuulApplication {
 
 	public static void main(String[] args) {
