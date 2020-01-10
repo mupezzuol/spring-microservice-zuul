e1be031350523c0fe5bb6cce5f378858399464c9
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-10 18:50:19 -0300

    Upload Project

---
+++ src/main/java/br/com/microservice/zuul/SpringMicroserviceZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceZuulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/microservice/zuul/SpringMicroserviceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservice.zuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
